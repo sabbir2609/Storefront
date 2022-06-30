2ba4194d65f3eef67212fb0c393689061ef51a53
sabbir2609 <sabbirhasan2999@gmail.com>
2022-06-30 10:28:25 -0700

    model admin options

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from . import models
 
+# more: https://docs.djangoproject.com/en/4.0/ref/contrib/admin/
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    # more: https://docs.djangoproject.com/en/4.0/ref/contrib/admin/#modeladmin-options
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)

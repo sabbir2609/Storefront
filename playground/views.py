bf250746455a0d3b182a28ef620538073c808c9b
sabbir2609 <sabbirhasan2999@gmail.com>
2022-06-30 08:57:41 -0700

    fixes

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from store.models import Product
 
 def say_hello(request):
+    qs = Product.objects.all()
+    for product in qs:
+        print(product)
+
+        
     return render(request, 'hello.html', {'name': 'Sabbir'})

e3e4fc22da729aa72ab4d97f97d5ff86e6846767
sabbir2609 <sabbirhasan2999@gmail.com>
2022-06-30 10:35:44 -0700

    customize list page -> admin

--- store/admin.py
+++ store/admin.py
@@ -10,4 +10,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['unit_price']
     list_per_page = 10
 
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    # more: https://docs.djangoproject.com/en/4.0/ref/contrib/admin/#modeladmin-options
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 admin.site.register(models.Collection)

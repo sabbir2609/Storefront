c5a12bddfac49784e58ea76982139c92c3a77afd
sabbir2609 <sabbirhasan2999@gmail.com>
2022-06-30 06:42:53 -0700

    add postgres and migrate

--- core/settings.py
+++ core/settings.py
@@ -79,21 +79,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront',
+        'USER': 'postgres',
+        'PASSWORD': '2609',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'storefront',
-#         'USER': 'postgres',
-#         'PASSWORD': '2609',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
 
 
 # Password validation

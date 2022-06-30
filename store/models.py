a23a3a321fc2cf113e41b24af9de303634e14f23
sabbir2609 <sabbirhasan2999@gmail.com>
2022-06-29 19:57:04 -0700

    models added to store

--- store/models.py
+++ store/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'B'
+    MEMBERSHIP_GOLD = 'B'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11)
+    birth_date = models.DateField(null=True)
+
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAIELD = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Compleate'),
+        (PAYMENT_STATUS_FAIELD, 'Failed'),
+
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1 ,choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    

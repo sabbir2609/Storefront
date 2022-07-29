c9fb9a0265acf31dab21f37fa1718e8174e8a229
sabbir2609 <sabbirhasan2999@gmail.com>
2022-07-29 16:37:58 +0600

    custom serializer field

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.db.models import CharField
+
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -7,16 +9,16 @@ class Promotion(models.Model):
     # products
 
 
-
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
-    
-    def __str__(self) -> str:
+
+    def __str__(self) -> CharField:
         return self.title
 
     class Meta:
         ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -24,16 +26,16 @@ class Product(models.Model):
     description = models.TextField()
     unit_price = models.DecimalField(
         max_digits=6,
-         decimal_places=2,
-         validators=[MinValueValidator(1)]
-         )
+        decimal_places=2,
+        validators=[MinValueValidator(1)]
+    )
     inventory = models.IntegerField(validators=[MinValueValidator(1)])
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
     promotions = models.ManyToManyField(Promotion, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self) -> CharField:
         return self.title
 
     class Meta:
@@ -58,37 +60,35 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
 
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-    
+
     def __str__(self) -> str:
-            return f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name}'
+
     # https://docs.djangoproject.com/en/4.0/ref/models/options/
-    
+
     class Meta:
         indexes = [
             models.Index(fields=['last_name', 'first_name'])
         ]
 
-    
-
-
 
 class Order(models.Model):
-
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAIELD = 'F'
 
     PAYMENT_STATUS_CHOICES = [
         (PAYMENT_STATUS_PENDING, 'Pending'),
-        (PAYMENT_STATUS_COMPLETE, 'Compleate'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
         (PAYMENT_STATUS_FAIELD, 'Failed'),
 
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(max_length=1 ,choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-    
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT) # never delete order
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)  # never delete order
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
@@ -102,17 +102,17 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     # make One-To_One Relationship with customer
     # customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
-    
+
     # If the customer has multiple addresses
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     zip = models.SmallIntegerField(null=True)
-    
-    
+
+
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
-
--- store/serializer.py
+++ store/serializer.py
@@ -1,8 +1,12 @@
-from operator import imod
-from unittest.util import _MAX_LENGTH
+from decimal import Decimal
 from rest_framework import serializers
+from .models import Product
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product : Product):
+        return product.unit_price * Decimal(1.1)

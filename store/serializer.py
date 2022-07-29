1753d11d4189fcf5de7f8352e67654c8e32d346f
sabbir2609 <sabbirhasan2999@gmail.com>
2022-07-29 16:41:33 +0600

    sz relationship

--- store/serializer.py
+++ store/serializer.py
@@ -1,12 +1,15 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset=Collection.objects.all()
+    )
 
     def calculate_tax(self, product : Product):
         return product.unit_price * Decimal(1.1)

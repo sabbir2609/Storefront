019295e6d5a2642b3a22929d80578a63612f51bf
sabbir2609 <sabbirhasan2999@gmail.com>
2022-07-31 01:12:04 +0600

    db heroku

--- core/settings.py
+++ core/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 if IS_HEROKU:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd4q9e2uqv8kt1a',
+            'USER': 'hlukwukzbywnhv',
+            'PASSWORD': '9284be841954166ffc920be7b2c4f43e6779810c18acad9d854ed87d15bb4f04',
+            'HOST': 'ec2-54-208-104-27.compute-1.amazonaws.com',
+            'PORT': '5432',
         }
     }
 else:

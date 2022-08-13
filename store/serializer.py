9ea875232c9c04840b5c2f89c8491c29a3062ded
sabbir2609 <sabbirhasan2999@gmail.com>
2022-08-13 15:37:11 +0600

    revart

--- store/serializer.py
+++ store/serializer.py
@@ -6,9 +6,8 @@ from .models import Product, Collection
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id','title','products_count']
-    products_count = serializers.IntegerField()
-    
+        fields = ['id','title']
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product

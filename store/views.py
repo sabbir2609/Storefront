1f187be9242884058aebbecdfdfd1e1021354669
sabbir2609 <sabbirhasan2999@gmail.com>
2022-08-14 23:01:36 +0600

    sorting

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models.aggregates import Count
 from .filters import ProductFilter
@@ -13,9 +13,10 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class =  ProductSerializer
     # for filtering
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}

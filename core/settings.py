32455c33216749f6957c5fa4a5775f1272e74de4
sabbir2609 <sabbirhasan2999@gmail.com>
2022-08-15 00:33:57 +0600

    doing some shit..  gorib hole ja hoy 🙄

--- core/settings.py
+++ core/settings.py
@@ -105,25 +105,34 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # default
 if IS_HEROKU:
     DATABASES = {
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.postgresql',
+        #     'NAME': 'd4q9e2uqv8kt1a',
+        #     'USER': 'hlukwukzbywnhv',
+        #     'PASSWORD': '9284be841954166ffc920be7b2c4f43e6779810c18acad9d854ed87d15bb4f04',
+        #     'HOST': 'ec2-54-208-104-27.compute-1.amazonaws.com',
+        #     'PORT': '5432',
+        # }
+
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'd4q9e2uqv8kt1a',
-            'USER': 'hlukwukzbywnhv',
-            'PASSWORD': '9284be841954166ffc920be7b2c4f43e6779810c18acad9d854ed87d15bb4f04',
-            'HOST': 'ec2-54-208-104-27.compute-1.amazonaws.com',
-            'PORT': '5432',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
     }
 else:
     DATABASES = {
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.postgresql',
+        #     'NAME': 'storefront',
+        #     'USER': 'postgres',
+        #     'PASSWORD': '2609',
+        #     'HOST': '127.0.0.1',
+        #     'PORT': '5432',
+        # }
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'storefront',
-            'USER': 'postgres',
-            'PASSWORD': '2609',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
     }
 
 

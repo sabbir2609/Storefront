614b217d0795a1b5af52d82edb0f33d6364da3cd
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2022-09-02 19:00:22 +0600

    creating a order item

--- store/serializer.py
+++ store/serializer.py
@@ -1,6 +1,5 @@
-from dataclasses import fields
 from decimal import Decimal
-from pyexpat import model
+from django.db import transaction
 from rest_framework import serializers
 from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
 
@@ -117,9 +116,23 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
 
-        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+            (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
 
-        Order.objects.create(customer=customer)
+            cart_items = CartItem.objects \
+                                        .select_related('product') \
+                                        .filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order = order,
+                    product = item.product,
+                    unit_price=item.product.unit_price,
+                    quantity = item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            Cart.objects.filter(pk=cart_id).delete()

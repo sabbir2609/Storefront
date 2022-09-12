94ca4ba671f57b84462dfd75d6115e832c837b9e
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2022-09-12 14:53:18 +0600

    templated mail

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import TemplateView
 from django.core.mail import EmailMessage, BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 
 
 def test(request):
@@ -8,10 +9,16 @@ def test(request):
         # send_mail('subject', 'message', 'sabbir@sabbir.inc', ['sabbirhasan2999@gmail.com'])
         # mail_admins('subject', 'message', html_message='message')
 
-        message = EmailMessage(
-            'subject', 'message', 'sabbir@sabbir.inc', ['sabbirhasan2999@gmail.com'])
-        message.attach_file('home/static/images/demo.png')
-        message.send()
+        # message = EmailMessage(
+        #     'subject', 'message', 'sabbir@sabbir.inc', ['sabbirhasan2999@gmail.com'])
+        # message.attach_file('home/static/images/demo.png')
+        # message.send()
+
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Sabbir'}
+        )
+        message.send(['sabbirhasan2999@gmail.com'])
 
     except BadHeaderError:
         pass

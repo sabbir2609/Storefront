c06589a5de2f20c0f46e6c055ce9c55b67781860
sabbir2609 <sabbirhasan2999@gmail.com>
2022-09-14 00:44:07 +0600

    test 1 success

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,16 +1,13 @@
 from urllib import response
 from rest_framework.test import APIClient
 from rest_framework import status
+import pytest
 
 
+@pytest.mark.django_db
 class TestClassCollection:
+
     def test_if_user_is_anonymous_returns_401(self):
-        # AAA (Arrange, Act, Assert)
-        # Arrange
-
-        # Act
         client = APIClient()
-        client.post('/store/collections/', {'title': 'a'})
-
-        # Assert
+        response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED

f43df3906fdaa4052eb297d081f1174c5fbfe3a5
sabbir2609 <sabbirhasan2999@gmail.com>
2022-09-16 00:57:52 +0600

    data is valid ?  test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from urllib import response
 from rest_framework.test import APIClient
 from rest_framework import status
@@ -16,3 +17,17 @@ class TestClassCollection:
         client.force_authenticate(user={})
         response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_data_is_invalid_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+
+    def test_if_data_is_valid_returns_400(self):
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0

5f20cc54b147eeb6b761e0e354d24238ff4830cf
sabbir2609 <sabbirhasan2999@gmail.com>
2022-09-16 00:48:01 +0600

    test user is admin ?

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -10,3 +10,9 @@ class TestClassCollection:
         client = APIClient()
         response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN

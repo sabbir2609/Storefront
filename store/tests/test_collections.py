d6fa7d7488f69b8a1246d3197d7895e7828f2acd
sabbir2609 <sabbirhasan2999@gmail.com>
2022-09-16 19:00:36 +0600

    fixture

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,27 +1,39 @@
-from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
 
 
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+    return do_create_collection
+
+
 @pytest.mark.django_db
 class TestClassCollection:
-    def test_if_user_is_anonymous_returns_401(self, api_client):
-        response = api_client.post('/store/collections/', {'title': 'a'})
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
+        response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self, api_client):
-        api_client.force_authenticate(user={})
-        response = api_client.post('/store/collections/', {'title': 'a'})
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        authenticate()
+
+        response = create_collection({'title': 'a'})
+
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self, api_client):
-        api_client.force_authenticate(user=User(is_staff=True))
-        response = api_client.post('/store/collections/', {'title': ''})
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+
+        response = create_collection({'title': ''})
+
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_returns_400(self, api_client):
-        api_client.force_authenticate(user=User(is_staff=True))
-        response = api_client.post('/store/collections/', {'title': 'a'})
+    def test_if_data_is_valid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+
+        response = create_collection({'title': 'a'})
+
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0

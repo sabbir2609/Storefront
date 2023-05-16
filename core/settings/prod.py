8a975a622302864c2fe1ecc1b4d9bf9f0cdf5aed
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-05-16 10:11:01 +0600

    lets see

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,45 +1,47 @@
 import os
+
 from .common import *
+
 # import dj_database_url
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+# Configure the domain name using the environment variable
+# that Azure automatically creates for us.
+ALLOWED_HOSTS = (
+    [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+)
+CSRF_TRUSTED_ORIGINS = (
+    ["https://" + os.environ["WEBSITE_HOSTNAME"]]
+    if "WEBSITE_HOSTNAME" in os.environ
+    else []
+)
 
 DEBUG = False
 
-# SECRET_KEY = os.environ['SECRET_KEY']
+# Configure Postgres database; the full username for PostgreSQL flexible server is
+# username (not @sever-name).
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$&ovi=euv((gdjh1xbuck7ou3rzj1xa*xa%zn6sindeh70gmbp')
-
-# ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-
-ALLOWED_HOSTS = ["storefrontx.azurewebsites.net"]
-
-# CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-
-CSRF_TRUSTED_ORIGINS = "storefrontx.azurewebsites.net"
-
-
-# Configure Postgres database; the full username for PostgreSQL flexible server is 
-# username (not @sever-name). 
-
-hostname = os.environ['DBHOST']
+hostname = os.environ["DBHOST"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'PORT': '5432',
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'],
-        "OPTIONS" : {"sslmode" : "require"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DBNAME"],
+        "PORT": "5432",
+        "HOST": hostname + ".postgres.database.azure.com",
+        "USER": os.environ["DBUSER"],
+        "PASSWORD": os.environ["DBPASS"],
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
-REDIS_URL = os.environ['REDIS_URL']
-REDIS_USER = os.environ['REDIS_USER']
-REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+REDIS_URL = os.environ["REDIS_URL"]
+REDIS_USER = os.environ["REDIS_USER"]
+REDIS_PASSWORD = os.environ["REDIS_PASSWORD"]
 
 # redis://username:password@host:port/db
-CELERY_BROKER_URL = f'redis://{REDIS_USER}:{REDIS_PASSWORD}@{REDIS_URL}'
+CELERY_BROKER_URL = f"redis://{REDIS_USER}:{REDIS_PASSWORD}@{REDIS_URL}"
 
 CACHES = {
     "default": {
@@ -49,12 +51,12 @@ CACHES = {
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "PASSWORD": REDIS_PASSWORD,
-        }
+        },
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ['SMTP_SERVER']
-EMAIL_HOST_USER = os.environ['SMTP_LOGIN']
-EMAIL_HOST_PASSWORD = os.environ['SMTP_PASSWORD']
-EMAIL_PORT = os.environ['SMTP_PORT']
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ["SMTP_SERVER"]
+EMAIL_HOST_USER = os.environ["SMTP_LOGIN"]
+EMAIL_HOST_PASSWORD = os.environ["SMTP_PASSWORD"]
+EMAIL_PORT = os.environ["SMTP_PORT"]

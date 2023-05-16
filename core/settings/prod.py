afc1a6d1c8cdb3fc249031be282ba5825834ccab
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-05-16 22:43:36 +0600

    refactor

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -9,14 +9,9 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = (
-    [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
-)
-CSRF_TRUSTED_ORIGINS = (
-    ["https://" + os.environ["WEBSITE_HOSTNAME"]]
-    if "WEBSITE_HOSTNAME" in os.environ
-    else []
-)
+ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+
+CSRF_TRUSTED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
 
 DEBUG = False
 

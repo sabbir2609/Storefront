054d995bf9cbea6cbc5e308359079b323e01cbb8
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-05-18 02:26:07 +0600

    Delete resource directory

--- resource/start.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# Start PostgreSQL
-postgre_process = subprocess.Popen(["sudo", "service", "postgresql", "start"])
-postgre_process.communicate()
-
-# Start Redis
-redis_process = subprocess.Popen(["sudo", "service", "redis-server", "start"])
-redis_process.communicate()

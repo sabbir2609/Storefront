e2dc173112b89d7dd55db9ea27ea4a7bb71df2eb
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-05-19 16:30:46 +0600

    Update asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# settings_module = 'core.settings.prod' if 'WEBSITE_HOSTNAME' in os.environ else 'core.settings.dev'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.dev')
 
 application = get_asgi_application()
